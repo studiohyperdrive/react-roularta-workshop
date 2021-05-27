c9a8c3f61b050f40dec32c295f8d0323e7b9159e
Andry Charlier <andrycharlier@gmail.com>
2021-05-27 09:12:38 +0200

    feat: add custom eslint config with vscode settings

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	// Extend from default CRA eslint config
+	extends: ['react-app'],
+	rules: {
+		'comma-dangle': ['error', 'always-multiline'],
+		indent: ['warn', 'tab'],
+		quotes: ['warn', 'single'],
+		semi: ['error', 'always'],
+
+		'react/self-closing-comp': 'warn',
+	},
+};
