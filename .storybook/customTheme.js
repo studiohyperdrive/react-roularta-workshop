560d236a2652ebbc87466b3508818d3faeddbf8d
Andry Charlier <andrycharlier@gmail.com>
2021-06-04 08:44:47 +0200

    feat: add custom theme

---
+++ .storybook/customTheme.js
@@ -0,0 +1,34 @@
+import { create } from '@storybook/theming';
+
+export default create({
+	base: 'dark',
+
+	// UI
+	appBg: 'rgba(40,44,52,1)',
+	// appContentBg: 'silver',
+	// appBorderColor: 'grey',
+	// appBorderRadius: 4,
+
+	// Typography
+	fontBase: '"Open Sans", sans-serif',
+	fontCode: 'monospace',
+
+	// Text colors
+	// textColor: 'black',
+	// textInverseColor: 'rgba(255,255,255,0.9)',
+
+	// Toolbar default and active colors
+	barTextColor: 'white',
+	// barSelectedColor: '#61dafb',
+	// barBg: '#282c34',
+
+	// Form colors
+	// inputBg: 'white',
+	// inputBorder: 'silver',
+	// inputTextColor: 'black',
+	// inputBorderRadius: 4,
+
+	brandTitle: 'Styleguide',
+	brandUrl: 'https://reactjs.org',
+	// brandImage: '/logo192.png',
+});
