0da602e2819d179f85961b6721e1fdde552f3fc4
Andry Charlier <andrycharlier@gmail.com>
2021-06-04 08:45:18 +0200

    feat(Button): add variants and mdx stories

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,9 +2,14 @@ import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import styles from './Button.module.scss';
 
-const Button = ({ className, label, onClick, type = 'button' }) => {
+const Button = ({ className, label, onClick, type = 'button', variant }) => {
+	const buttonClassNames = clsx(className, styles['c-button'], {
+		[styles['c-button--dark']]: variant === 'dark',
+		[styles['c-button--light']]: variant === 'light',
+	});
+
 	return (
-		<button className={clsx(className, styles['c-button'])} onClick={onClick} type={type}>
+		<button className={buttonClassNames} onClick={onClick} type={type}>
 			{label}
 		</button>
 	);
@@ -15,6 +20,7 @@ Button.propTypes = {
 	label: PropTypes.string.isRequired,
 	onClick: PropTypes.func,
 	type: PropTypes.oneOf(['button', 'submit', 'reset']),
+	variant: PropTypes.oneOf(['dark', 'light']),
 };
 
 export default Button;
--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -5,7 +5,20 @@ const Template = (args) => <Button {...args} />;
 export const Default = Template.bind({});
 Default.args = { label: 'Button' };
 
+export const Dark = Template.bind({});
+Dark.args = { ...Default.args, variant: 'dark' };
+
+export const Light = Template.bind({});
+Light.args = { ...Default.args, variant: 'light' };
+
 export default {
 	title: 'Components/Button',
 	component: Button,
+	parameters: {
+		docs: {
+			description: {
+				component: 'Button UI component',
+			},
+		},
+	},
 };
